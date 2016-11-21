081fc30b7b5d1b57ad4d54990b34348e51ab8410
c_sreddycharla <vaishnavireddycharla@gmail.com>
2016-11-21 15:00:29 +0530

    Getjson

---
+++ Ajax1/emp.js
@@ -0,0 +1,5 @@
+{"employees":[
+    {"firstName":"vaishnavi", "lastName":"reddycharla" , "experience":"1" },
+    {"firstName":"Deepika", "lastName":"Padam" , "experience":"3"},
+    {"firstName":"vaishali", "lastName":"reddycharla" ,  "experience":"4"}
+]}
